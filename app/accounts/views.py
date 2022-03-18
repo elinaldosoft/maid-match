a6746db307c8973075bcaedbc3a63f387ed2aaf4
Elinaldo Monteiro <elinaldosoft@gmail.com>
2022-03-17 22:09:30 -0300

    Create block and base.html to accounts

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -2,11 +2,10 @@ from django.views import View
 from django.http.request import HttpRequest
 from django.http.response import HttpResponse
 from django.shortcuts import render
-# Create your views here.
+
 
 class AccountsView(View):
-    template_name = 'login.html'
-    
+    template_name = 'accounts/index.html'
+
     def get(self, request: HttpRequest) -> HttpResponse:
         return render(request, self.template_name)
-               
