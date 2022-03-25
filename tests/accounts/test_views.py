4a7d5d584057d3e2aa63acacb2db9721d95bca55
Edmilson Machado Pereira <edmilsonill@hotmail.com>
2022-03-24 23:00:35 -0300

    teste da tela registro

--- tests/accounts/test_views.py
+++ tests/accounts/test_views.py
@@ -1,17 +1,26 @@
 import pytest
 from django.urls import reverse
 
+@pytest.mark.django_db
+def test_accounts(client):
+    url = reverse('accounts')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.charset == 'utf-8'
+
+    html = response.content.decode()
+    assert 'Login' in html
+    assert response.headers
+    assert response.headers.get('Content-Type') == 'text/html; charset=utf-8'
 
 @pytest.mark.django_db
-class TestAccounts:
+def test_register(client):
+    url = reverse('register')
+    response = client.get(url)
+    assert response.status_code == 200
+    assert response.charset == 'utf-8'
 
-    def test_render_get(self, client):
-        url = reverse('accounts')
-        response = client.get(url)
-        assert response.status_code == 200
-        assert response.charset == 'utf-8'
-
-        html = response.content.decode()
-        assert 'Login' in html
-        assert response.headers
-        assert response.headers.get('Content-Type') == 'text/html; charset=utf-8'
+    html = response.content.decode()
+    assert 'Cadastro' in html
+    assert response.headers
+    assert response.headers.get('Content-Type') == 'text/html; charset=utf-8'
